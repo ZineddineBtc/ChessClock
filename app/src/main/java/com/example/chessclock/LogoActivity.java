3a6adaf1d2c5d36c8c9a5a80e7d33e15766fd8d9
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-16 19:48:17 +0100

    LogoActivity adjusted

--- app/src/main/java/com/example/chessclock/LogoActivity.java
+++ app/src/main/java/com/example/chessclock/LogoActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.Objects;
+
 public class LogoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logo);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
